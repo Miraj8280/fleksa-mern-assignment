2a0c30af8df3329f32464173db3f06ce7c672ece
Miraj Asraf <mirajasraf786@gmail.com>
2023-07-05 19:31:09 +0530

    Register and Login - Auth controll created

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
 const app = express();
+const authController = require('./controllers/AuthController');
 
 /* CONNECT TO MONGODB */
 mongoose.connection.on('error', (err) => {
@@ -20,6 +21,17 @@ mongoose.connect(process.env.MONGO_URL, {
   .catch((err) => {
     console.error('MongoDB connection error:', err);
   });
+
+
+/* ROUTES & MIDDLEWARES */
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/images', express.static('public/images'));
+app.use('/auth', authController);
+// app.use('/product', productController);
+// app.use('/upload', uploadController);
+
 
 /* START THE SERVER */
 const port = process.env.PORT || 5000;
